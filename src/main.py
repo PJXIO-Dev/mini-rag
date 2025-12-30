01b47fc75642c200abfcfe68b6c71e13661a92ff
PJXIO-Sabbahi <142024439+PJXIO-Dev@users.noreply.github.com>
2025-12-30 11:50:32 +0300

    move code into src folder

--- src/main.py
+++ src/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
-
 from dotenv import load_dotenv
-load_dotenv(".env")
-
 from routes import base
 
+load_dotenv(".env")
 
 app = FastAPI()
-
-
 app.include_router(base.base_router)
